49085670cd5cf1bc59404d20f4ec68168ab2302d
Daniel Moradi <Daniel.moradi15@gmail.com>
2026-05-05 21:23:35 +0200

    feat: seting up the frontend interface for Train res data

---
+++ frontend/src/services/trainService.ts
@@ -0,0 +1,45 @@
+import type { b } from "vue-router/dist/index-D_VEAp3P.js";
+
+// URL for the backend API
+const API_URL = 'http://localhost:8080/api/api';
+
+export interface Location {
+    LocationName: string
+    Priority: number 
+    Order: number
+}
+
+
+export interface Train {
+    ActivityId: string
+    ActivityType: string
+    Advertised: boolean
+    AdvertisedTimeAtLocation: string
+    AdvertisedTrainIdent: string // tågnummer 7151
+    Canceled: boolean
+    Deleted: boolean
+    FromLocation: Location[]
+    ToLocation: Location[]
+    ViaToLocation: Location[]
+    LocationSignature: string // stationskod t.ex. "Sjka"
+    InformationOwner: string
+    Operator: string
+    TrainOwer: string // tågägare
+    TrackAtLocation: string // spårnummer
+    ModifiedTime: string
+}
+
+
+// funktioen som hämtar alla tåg från backend 
+export async function fetchTrains(date: string): Promise<Train[]> {
+    const response = await fetch(`${API_URL}/trains?date=${date}`)
+
+    if (!response.ok){
+        throw new Error('Failed to fetch trains')
+    }
+
+    // svar som kommer från backend är rå text
+    const data = await response.json()
+    // returnerar bara listan av tåg
+    return data.trains
+}
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,3 +1,22 @@
+<script setup lang = "ts">
+import { ref, onMounted } from 'vue'
+import {fetchTrains, type Train} from '../services/trainService'
+
+// Dagens datum 
+const today = new Date().toISOString().split('T')[0]
+
+// Reaktiva variabler
+const trains = ref<Train[]>([])
+const date = ref(today)
+const loading = ref(false)
+const error = ref('')
+
+// hämta tåg från backend api
+
+
+
+</script>
+
 <template>
   <main>
   <h1>Trafikverket</h1>
